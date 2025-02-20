6f389bcea344439e38f508be0c6f09733b1cd445
Leonardo Moreira <lm.moreira@gmail.com>
2025-02-20 16:18:45 -0300

    Fix

--- src/test/java/br/com/logistics/tms/TmsApplicationTests.java
+++ src/test/java/br/com/logistics/tms/TmsApplicationTests.java
@@ -3,9 +3,11 @@ package br.com.logistics.tms;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
+@TestPropertySource(locations = "classpath:env-test")
 class TmsApplicationTests {
 
 	@Test
